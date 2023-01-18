2a69c53bb46f7132b50897668820e4a297d5bb78
Harit Khushwas <haritchandel1234@gmail.com>
2023-01-18 16:18:08 +0530

    day_1 18 jan

---
+++ Course_Management_System/src/com/DBUtil/DBUtil.java
@@ -0,0 +1,29 @@
+package com.DBUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.Model.Course;
+
+public class DBUtil {
+	public Connection provideConnection(Course course) {
+		Connection conn = null;
+		
+		 try {
+			 Class.forName("com.mysql.cj.jdbc.Driver");
+		 } catch(ClassNotFoundException e) {
+			 e.printStackTrace();
+		 }
+		 
+		String url = "jdbc:mysql://localhost:3306/course_management";
+		
+		try {
+		conn = DriverManager.getConnection(url,"root","Harit@274294");
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
